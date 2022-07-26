481e1468b647c5f2218213cd24f821ffdc4667aa
Vaidas Budrys <vaidas.budrys3@gmail.com>
2022-07-26 18:48:20 -0400

    enable auto lines

--- main.py
+++ main.py
@@ -103,7 +103,7 @@ if __name__ == "__main__":
     lcd = CharLCD(i2c_expander='PCF8574', address=0x27, port=1,
                   cols=20, rows=4, dotsize=8,
                   charmap='A02',
-                  auto_linebreaks=False,
+                  auto_linebreaks=True,
                   backlight_enabled=True)
 
     weather = generate_display_message(locations[0])
