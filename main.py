67166cf9612b78e1d522286f64a0a9e38ffcf010
Vaidas Budrys <vaidas.budrys3@gmail.com>
2022-07-26 18:30:50 -0400

    add test code

--- main.py
+++ main.py
@@ -104,4 +104,45 @@ if __name__ == "__main__":
     i2c = board.I2C()
     lcd = character_lcd.Character_LCD_I2C(i2c, LCD_COLUMNS, LCD_ROWS, address=0x27)
     # lcd.backlight = True
-    lcd.message = "Hello world" # generate_display_message(locations[0])
+    # lcd.message = "Hello world" # generate_display_message(locations[0])
+    # Turn backlight on
+    lcd.backlight = True
+    # Print a two line message
+    lcd.message = "Hello\nCircuitPython"
+    # Wait 5s
+    time.sleep(5)
+    lcd.clear()
+    # Print two line message right to left
+    lcd.text_direction = lcd.RIGHT_TO_LEFT
+    lcd.message = "Hello\nCircuitPython"
+    # Wait 5s
+    time.sleep(5)
+    # Return text direction to left to right
+    lcd.text_direction = lcd.LEFT_TO_RIGHT
+    # Display cursor
+    lcd.clear()
+    lcd.cursor = True
+    lcd.message = "Cursor! "
+    # Wait 5s
+    time.sleep(5)
+    # Display blinking cursor
+    lcd.clear()
+    lcd.blink = True
+    lcd.message = "Blinky Cursor!"
+    # Wait 5s
+    time.sleep(5)
+    lcd.blink = False
+    lcd.clear()
+    # Create message to scroll
+    scroll_msg = "<-- Scroll"
+    lcd.message = scroll_msg
+    # Scroll message to the left
+    for i in range(len(scroll_msg)):
+        time.sleep(0.5)
+        lcd.move_left()
+    lcd.clear()
+    lcd.message = "Going to sleep\nCya later!"
+    time.sleep(5)
+    # Turn backlight off
+    lcd.backlight = False
+    time.sleep(2)
